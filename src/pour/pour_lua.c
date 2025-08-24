eadc883d7411b4b1d9439b423f99b09be07c2986
Drunk Fly Team <drunkflyteam@gmail.com>
2025-08-24 21:43:47 +0300

    Added 'pour.run' command.

--- src/pour/pour_lua.c
+++ src/pour/pour_lua.c
@@ -9,10 +9,27 @@ static int pour_require(lua_State* L)
     return 0;
 }
 
+static int pour_run(lua_State* L)
+{
+    int argc = lua_gettop(L);
+    const char* package = luaL_checkstring(L, 1);
+
+    char** argv = (char**)lua_newuserdata(L, argc * sizeof(char**));
+    argv[0] = (char*)package;
+    for (int i = 1; i < argc; i++)
+        argv[i] = (char*)luaL_checkstring(L, i + 1);
+
+    if (!Pour_Run(package, argc, argv))
+        return luaL_error(L, "command execution failed.");
+
+    return 0;
+}
+
 /********************************************************************************************************************/
 
 static const luaL_Reg funcs[] = {
     { "require", pour_require },
+    { "run", pour_run },
     { NULL, NULL }
 };
 
