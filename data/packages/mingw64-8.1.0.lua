0d5f2917599334d89f6fc6d64ac0ddd0b9a2c382
Drunk Fly Team <drunkflyteam@gmail.com>
2025-08-25 01:43:48 +0300

    Added packages for 32-bit and 64-bit mingw 8.1.0.

---
+++ data/packages/mingw64-8.1.0.lua
@@ -0,0 +1,13 @@
+
+if WINDOWS then
+    SOURCE_URL = 'https://github.com/thirdpartystuff/win32-mingw810-64'
+    TARGET_DIR = TOOLS_DIR..'/win32-mingw810-64'
+    EXTRA_PATH = { TARGET_DIR..'/bin' }
+    EXECUTABLE = {
+        _default_ = 'gcc',
+        ['ar'    ] = TARGET_DIR..'/bin/ar.exe',
+        ['ranlib'] = TARGET_DIR..'/bin/ranlib.exe',
+        ['gcc'   ] = TARGET_DIR..'/bin/gcc.exe',
+        ['g++'   ] = TARGET_DIR..'/bin/g++.exe',
+    }
+end
