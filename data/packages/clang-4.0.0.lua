19f405a6cb258cd531029aa12b6dbfda119dcd9a
Drunk Fly Team <drunkflyteam@gmail.com>
2025-08-24 22:11:14 +0300

    Added package definition for clang 4.0.0.

---
+++ data/packages/clang-4.0.0.lua
@@ -0,0 +1,10 @@
+
+if WINDOWS then
+    SOURCE_URL = 'https://github.com/thirdpartystuff/win32-clang-4.0.0'
+    TARGET_DIR = TOOLS_DIR..'/win32-clang-4.0.0'
+    EXTRA_PATH = { TARGET_DIR..'/bin' }
+    EXECUTABLE = {
+        _default_ = 'clang',
+        clang = TARGET_DIR..'/bin/clang.exe',
+    }
+end
