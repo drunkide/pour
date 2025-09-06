526fe2ba31690f6579a3d4f462db6739a981909b
Drunk Fly Team <drunkflyteam@gmail.com>
2025-09-07 01:39:17 +0300

    Fix URL.

--- data/packages/vm-dos.lua
+++ data/packages/vm-dos.lua
@@ -1,4 +1,4 @@
 
-SOURCE_URL = 'git@github.com:thirdpartystuff/vm-dos'
+SOURCE_URL = 'https://github.com/thirdpartystuff/vm-dos'
 TARGET_DIR = INSTALL_DIR..'/vm-dos'
 INVOKE_LUA = TARGET_DIR..'/config.lua'
